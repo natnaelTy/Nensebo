b5389bb35e9cc7e109e12e2071d0104b3ca6ed14
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-22 23:02:36 +0300

    just starting off

---
+++ app/about/Image.ts
@@ -0,0 +1,43 @@
+
+interface ImageType {
+  id: number,
+  image: string
+}
+
+
+const images = <ImageType[]> [
+    {
+      id: 0,
+      image: "./images/tade-7.jpg"
+    },
+    {
+      id: 1,
+      image: "./images/onsite-2.jpg"
+    },
+    {
+      id: 2,
+      image: "./images/onsite.jpg"
+    },
+    {
+      id: 3,
+      image: "./images/coffeeSite.JPG"
+    },
+    {
+      id: 4,
+      image: "./images/site1.jpg"
+    },
+    {
+      id: 5,
+      image: "./images/site4.jpg"
+    },
+    {
+      id: 6,
+      image: "./images/tade-8.jpg"
+    },
+    {
+      id: 7,
+      image: "./images/tade-9.jpg"
+    }
+  ]
+
+export default images;
