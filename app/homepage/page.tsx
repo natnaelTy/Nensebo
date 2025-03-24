fb65ede540a957d162cd7afb498cecaa2027f63b
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-24 22:34:09 +0300

    overflowing issue fixed

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   return (
     <>
       <div className="home relative">
-        <div className="md:flex flex-cols items-center gap-8 justify-around w-full h-screen p-4">
+        <div className="md:flex flex-cols items-center justify-around gap-4 min-h-screen pt-30 md:pt-0 w-full">
           <motion.div
             initial={{ opacity: 0, y: 150 }}
             whileInView={{ opacity: 1, y: 0 }}
@@ -43,7 +43,7 @@ export default function Home() {
             className="flex flex-col items-center md:items-start justify-center p-3 gap-3 text-center md:text-left"
           >
             <h1 className="max-w-xl text-4xl text-white font-bold uppercase md:text-5xl z-10">
-              Nensebo <span className="text-green-600 font-extrabold">coffee </span>
+              Nensebo <span className="text-green-700 font-extrabold">coffee </span>
               Supply
             </h1>
             <h2 className={`${nunitoSans.className}text-xl text-white font-semibold z-10 md:text-2xl`}>
@@ -54,13 +54,13 @@ export default function Home() {
               Ethiopia’s coffee-producing regions, focusing on global
               distribution.
             </p>
-            <button className="bg-white border-2 border-white px-4 py-3 text-sm rounded-lg uppercase font-semibold mt-8 hover:bg-transparent hover:text-white z-10">
+            <button className="bg-transparent text-white font-bold border-2 border-white px-4 py-3 text-sm rounded-lg uppercase mt-8 hover:bg-white hover:text-black z-10">
               <Link href="/contact">Contact Us</Link>
             </button>
           </motion.div>
           
           <div className="relative
-          max-w-sm w-full h-auto z-10">
+         max-w-xs md:max-w-sm w-full h-auto z-10">
             <img src="/images/banner_coffee_bag.png" alt="" className="object-cover w-full h-full" />
           </div>
           {images.length > 0 &&
