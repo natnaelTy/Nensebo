b247f788ff2841f85e23fd617ee92d6ac45b6cb4
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-26 22:59:24 +0300

    lil change with the styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import HomePage from "./homepage/page";
 import WhatWeOffer from "./WhatWeOffer/page";
-
+import ShortAbout from "./shortAbout/page";
 
 export default function Home() {
   return (
         <>
           <HomePage/>
+          <ShortAbout/>
           <WhatWeOffer/>
         </>
   );
