593750e1b624c194ca23845e7d43370c45063590
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-26 23:03:30 +0300

    new way of animation and made all containers same height

--- app/WhatWeOffer/page.tsx
+++ app/WhatWeOffer/page.tsx
@@ -3,9 +3,13 @@ import { motion } from "framer-motion";
 import data from "./data";
 
 export default function WhatWeOffer() {
+
   return (
     <>
-      <div className="mb-20">
+      <div className="mb-20 relative">
+        <div className="max-w-xs h-auto absolute top-0 right-16 opacity-20 z-[-1] transalte origin-bottom -rotate-32">
+           <img src="./images/coffeeplant.png" alt="" className="w-full h-full object-cover"/>
+        </div>
         <motion.div
           initial={{ opacity: 0, y: 40 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -13,21 +17,23 @@ export default function WhatWeOffer() {
           viewport={{ once: true }}
           className="relative"
         >
-          <h1 className="text-center text-2xl font-normal mt-16">
+          <h1 className="text-center text-xl sm:text-2xl lg:text-3xl font-normal mt-16">
             What we offer
           </h1>
-          <div className="marked"></div>
+          <div className="underlineForTitle"></div>
         </motion.div>
-        <div className="flex  justfy-center flex-wrap gap-8 w-full h-full mt-10 p-4">
+
+        <div className="flex justify-center flex-wrap gap-8 w-full h-full mt-10 p-4">
           {data.length > 0 &&
             data.map((card, index) => (
               <motion.div
                 initial={{ opacity: 0, y: 70 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1.5 }}
+                transition={{ duration: 1.5, delay: index * 0.3 }} // Stagger animation
                 viewport={{ once: true }}
                 className="WhatWeOfferCard"
-                key={card.id}>
+                key={card.id}
+              >
                 <img
                   src={card.imageUrl}
                   alt=""
