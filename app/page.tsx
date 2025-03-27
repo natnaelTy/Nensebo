39ebdcf708aa80803be7093ab8daa76c89ca55d3
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-28 00:03:49 +0300

    new components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import HomePage from "./homepage/page";
 import WhatWeOffer from "./WhatWeOffer/page";
 import ShortAbout from "./shortAbout/page";
+import WhyChooseUs from "./whychooseus/page";
+import OurSites from "./oursites/page";
 
 export default function Home() {
   return (
         <>
           <HomePage/>
           <ShortAbout/>
+          <WhyChooseUs/>
           <WhatWeOffer/>
+          <OurSites/>
         </>
   );
 }
