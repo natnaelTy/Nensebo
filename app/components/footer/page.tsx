4e6b327a47fe8241885a32ce56792231bc9d5720
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-03-28 23:18:33 +0300

    implemented utility class's

--- app/components/footer/page.tsx
+++ app/components/footer/page.tsx
@@ -9,64 +9,65 @@ import { TfiLocationPin } from "react-icons/tfi";
 
 function Footer() {
   return (
-    <div className="bg-green-950 flex flex-col items-start justify-around md:flex-row p-4 py-10 gap-10 md:gap-0 text-white">
-        <div className="flex items-start flex-col justify-start">
-          <div className="w-full flex items-center justify-start p-2">
-            <img
-              src="./images/coffeelogo.png"
-              alt=""
-              className="w-[70px] object-cover mb-6"
-            />
-            <h1 className="text-lg md:text-xl font-medium">Sidama Nensebo</h1>
-          </div>
-          <p>
-            Speciality coffee from the land <br /> of origin.
-          </p>
+    <div className="bg-green-950 p-4">
+    <div className="flex flex-col items-start justify-around md:flex-row p-4 py-10 gap-10 md:gap-0 text-white">
+      <div className="flex items-start flex-col justify-start">
+        <div className="w-full flex items-center justify-start p-2">
+          <img
+            src="./images/coffeelogo.png"
+            alt=""
+            className="w-[70px] object-cover mb-6"
+          />
+          <h1 className="text-lg md:text-xl font-semibold">Sidama Nensebo</h1>
         </div>
+        <p>
+          Speciality coffee from the land <br /> of origin.
+        </p>
+      </div>
 
-        <div className="LinkBox">
-          <h1 className="footerHeaderText">Usefull Links</h1>
-          <ul className="unorderList">
-            <button>
-              <Link href={"/"}>Home</Link>
-            </button>
-            <button>
-              <Link href={"/aboutUs"}>About</Link>
-            </button>
-            <a href="">What we offer</a>
-            <button>
-              <Link href={"/ordering-info"}>Info (FAQ)</Link>
-            </button>
-            <button>
-              <Link href={"/contact"}>Contact</Link>
-            </button>
-          </ul>
-        </div>
-        
-        <div className="LinkBox">
-          <h1 className="footerHeaderText">Socials</h1>
-          <ul className="unorderList text-xl">
-            <li className="hover:text-blue-700">
-              <a href="https://www.facebook.com/tadesse.degife.1?mibextid=ZbWKwLhttps://www.facebook.com/tadesse.degife.1?mibextid=ZbWKwL">
-                <FaFacebook />
-              </a>
-            </li>
-            <li className="hover:text-green-500">
-              <a href="">
-                <IoLogoWhatsapp />
-              </a>
-            </li>
-            <li className="hover:text-blue-700 ">
-              <a href="https://www.linkedin.com/in/tade-coffee-export-847341358/">
-                <IoLogoLinkedin />
-              </a>
-            </li>
-          </ul>
-        </div>
+      <div className="LinkBox">
+        <h1 className="footerHeaderText">Usefull Links</h1>
+        <ul className="unorderList">
+          <button>
+            <Link href={"/"}>Home</Link>
+          </button>
+          <button>
+            <Link href={"/aboutUs"}>About</Link>
+          </button>
+          <a href="">What we offer</a>
+          <button>
+            <Link href={"/ordering-info"}>Info (FAQ)</Link>
+          </button>
+          <button>
+            <Link href={"/contact"}>Contact</Link>
+          </button>
+        </ul>
+      </div>
 
-        <div className="LinkBox">
-          <h1 className="footerHeaderText">Contact us</h1>
-          <div className="unorderList">
+      <div className="LinkBox">
+        <h1 className="footerHeaderText">Socials</h1>
+        <ul className="unorderList text-xl">
+          <li className="hover:text-blue-700">
+            <a href="https://www.facebook.com/tadesse.degife.1?mibextid=ZbWKwLhttps://www.facebook.com/tadesse.degife.1?mibextid=ZbWKwL">
+              <FaFacebook />
+            </a>
+          </li>
+          <li className="hover:text-green-500">
+            <a href="">
+              <IoLogoWhatsapp />
+            </a>
+          </li>
+          <li className="hover:text-blue-700 ">
+            <a href="https://www.linkedin.com/in/tade-coffee-export-847341358/">
+              <IoLogoLinkedin />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="LinkBox">
+        <h1 className="footerHeaderText">Contact us</h1>
+        <div className="unorderList">
           <div className="footerContactCard">
             <PiPhoneIncomingThin className="text-2xl" />
             <p>+251 911828573</p>
@@ -83,8 +84,11 @@ function Footer() {
             <TfiLocationPin className="text-2xl" />
             <p>Sarbet, Addis Ababa</p>
           </div>
-          </div>
         </div>
+      </div>
+    </div>
+    <hr className="text-gray-200 p-2 max-w-[1200px] mx-auto"/>
+    <div className="text-white py-3 text-xs md:text-sm text-center">Copyright © 2025. All right reserved. Sidama Nensebo</div>
     </div>
   );
 }
