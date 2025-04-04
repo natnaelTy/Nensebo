46d4e75c3de5aa56a79febbc5f1bfe98ff841723
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-04-04 22:11:10 +0300

    just for testing purpose

--- app/components/navbar/DropDown.tsx
+++ app/components/navbar/DropDown.tsx
@@ -25,31 +25,13 @@ function DropDownList () {
 
 const DropDown: React.FC<NavbarProps> = ({showMenu, handleShowMenu}) => {
 
-    const menuVar: Variants = {
-        initial:{
-            scaleY: 0,
-        },
-        animate:{
-            scaleY: 1,
-            transition:{
-                duration: 0.5,
-                ease: [0.12, 0, 0.39, 0]
-            }
-        },
-        exit:{
-            scaleY: 0,
-            transition:{
-                duration: 0.8,
-                ease: [0.22, 1, 0.36, 1]
-            }
-        }
-    }
+  
     
     return(
         <>
          <AnimatePresence>
                  {showMenu && (
-                    <motion.div variants={menuVar}
+                    <motion.div 
                                 initial="initial"
                                 animate="animate"
                                 exit="exit"
