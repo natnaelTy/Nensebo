4d4447e269e800b6c493499f09282e8b85499186
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-04-05 21:13:02 +0300

    new layout

--- app/oursites/page.tsx
+++ app/oursites/page.tsx
@@ -2,62 +2,58 @@
 import Image from "next/image";
 import OurSite from "./data";
 import Link from "next/link";
+import { MdPushPin } from "react-icons/md";
 
 
 function OurSites() {
   return (
-    <div className="text-center mb-8 p-4 relative">
+    <div className="bg-gray-100 pt-12 pb-12">
       {/* description */}
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold text-center">{`Our site's`}</h1>
+      <h1 className="text-xl md:text-2xl lg:text-3xl font-medium text-center">{`Our site's`}</h1>
       {/* description underline */}
       <div className="underlineForTitle"></div>
 
-      <div className="imageContainer">
-        <div className="contentContainer">
-          <div className="flex text-center gap-6 items-center justify-center w-full flex-col">
-            <h1 className="text-3xl font-semibold">
-              Discover Our Coffee Source
-            </h1>
-            <p className="">
-              Kerchanshe sources coffee from nine different regions in Ethiopia
-              - Sidamo, Guji, Yirgacheffe, Limmu, Bale, Harar, Jimma, Nekemte,
-              Kaffa.
-            </p>
-          </div>
-
-          <div className="flex items-center justify-between gap-6 w-full">
-            <div className="flex items-start justify-between w-full text-left gap-2">
-              {OurSite.length > 0 &&
-                OurSite.map((data) => (
-                  <div
-                    key={data.id}
-                    className="flex flex-col gap-3 items-start"
-                  >
-                    <Image
-                      src={data.imageUrl}
-                      alt="image"
-                      width={300}
-                      height={250}
-                      className="rounded-md h-[250px] object-cover"
-                    />
-                    <h1 className="siteName">{data.siteName}</h1>
-                    <p className="siteDescription">{data.shortDescription}</p>
-                    <Link href={`/oursites/${data.id}`}>
-                      <button className="primarybtn mt-2">{data.btn}</button>
-                    </Link>
-                  </div>
-                ))}
-            </div>
-          </div>
+      <div className="flex items-center justify-between w-full flex-col md:flex-row p-4">
+        <div className="flex text-left gap-5 items-start justify-center flex-col">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">Discover Our Coffee Source</h1>
+          <p className="max-w-2xl">
+            Kerchanshe sources coffee from nine different regions in Ethiopia -
+            Sidamo, Guji, Yirgacheffe, Limmu, Bale, Harar, Jimma, Nekemte,
+            Kaffa.
+          </p>
+          <button className="bg-green-950 cursor-pointer px-4 py-2 text-white rounded-md">
+            View All
+          </button>
         </div>
 
-        <Image
-          src="/images/homepic.jpg"
-          alt="coffee"
-          width={1200}
-          height={700}
-          className="w-full h-[700px] object-cover rounded-md"
-        />
+        <div className="flex items-start justify-center flex-col md:flex-wrap lg:flex-row w-full gap-10 md:gap-6 h-full mt-10">
+          {OurSite.length > 0 &&
+            OurSite.slice(0, 3).map((data) => (
+              <div
+                key={data.id}
+                className="overflow-hidden max-w-[280px]"
+              >
+                <Image
+                  src={data.imageUrl}
+                  alt="image"
+                  width={280}
+                  height={250}
+                  className="object-cover w-full rounded-t-xl h-52 "
+                />
+                <div className="flex items-start text-left flex-col p-3 gap-3 bg-gray-50 h-[250px]">
+                  <p className="text-sm font-medium text-gray-600 flex items-center gap-2"><MdPushPin/>{data.location}</p>
+                  <h1 className="siteName">{data.siteName}</h1>
+                  <p className="px-3 py-1 bg-gray-200 text-gray-900 rounded-full text-xs font-semibold">{data.coffeeType}</p>
+                  <p className="siteDescription">{data.shortDescription}</p>
+                  <Link href={`/oursites/${data.id}`}>
+                    <button className="border-1 border-gray-950 rounded-md px-3 py-2 text-sm mt-2">
+                      {data.btn}
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
