ad537ce93aee81397e29a00c768464dd936dad2c
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-04-05 21:05:38 +0300

    await the params

--- app/oursites/[oursitesId]/page.tsx
+++ app/oursites/[oursitesId]/page.tsx
@@ -1,20 +1,19 @@
 
  import { notFound } from "next/navigation";
  import OurSite from "../data";
-
+ import Image from "next/image";
 
   
- function SiteDetail({ params }: { params: {oursitesId: string } }) {
+ async function SiteDetail({ params }: { params: {oursitesId: string } }) {
 
-    const site =  OurSite.find((s) => s.id.toString() === params.oursitesId);
+    const site =  await OurSite.find((s) => s.id.toString() ===  params.oursitesId);
 
     if (!site) return notFound();
 
    return (
-     <div className="p-6 max-w-4xl mx-auto">
+     <div className="p-6 flex items-center justify-between w-full bg-tean-950">
     
-     <h1 className="text-3xl font-bold mt-4">page is {site.siteName}</h1>
-     
+      <Image src={site.imageUrl} alt={site.siteName} width={600} height={500}/> 
      </div>
    );
  }
