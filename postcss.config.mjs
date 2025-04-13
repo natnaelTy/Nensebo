f89dbbd40ffc1935bdf954ced638dc6e47e1f095
Natnael taye <152725896+natnaelTy@users.noreply.github.com>
2025-04-13 21:45:50 +0300

    final commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-module.exports = {
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
 
-export default config;
